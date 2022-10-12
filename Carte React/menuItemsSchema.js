da6eba79acce671879b563d7b0ff6386db17ad5c
Juan Asencio <107376595+juanasencio2009@users.noreply.github.com>
2022-10-11 21:25:33 -0600

    Add files via upload

--- Carte React/menuItemsSchema.js
+++ Carte React/menuItemsSchema.js
@@ -5,6 +5,31 @@ const menuItemSchema = Yup.object().shape({
     name: Yup.string().min(2, 'Minimum 2 character').max(100, 'Max 100 characters').required('Required'),
     description: Yup.string().max(500, 'max 500 characters'),
     imageUrl: Yup.string().url('Invalid image address').max(255, 'max 255 characters'),
+    orderStatus: Yup.number('Required').min(1, 'Int must be > 0').max(100, 'Exceeded max numbers').required('Required'),
+    menuFoodSafeType: Yup.array()
+        .of(
+            Yup.object().shape({
+                id: Yup.number().required(),
+                label: Yup.string().required(),
+            })
+        )
+        .required('Required'),
+    tags: Yup.array()
+        .of(
+            Yup.object().shape({
+                id: Yup.number().required(),
+                label: Yup.string().required(),
+            })
+        )
+        .required('Required'),
+    menuIngredients: Yup.array()
+        .of(
+            Yup.object().shape({
+                id: Yup.number().required(),
+                label: Yup.string().required(),
+            })
+        )
+        .required('Required'),
 });
 
 export default menuItemSchema;
